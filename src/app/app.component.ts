17a0c1606e2758e2148c7063135996074ea8a7ca
Sascha Nemeth <sascha.nemeth@me.com>
2024-02-05 17:08:13 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+// import { CommonModule} from "@angular/common";
+import {RouterOutlet} from '@angular/router';
+import {MainContentComponent} from "./main-content/main-content.component";
+import {FooterComponent} from "./shared/components/footer/footer.component";
+import {ImprintComponent} from "./imprint/imprint.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet,
+        MainContentComponent,
+        FooterComponent,
+        ImprintComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'sakura';
+    title = 'sakura';
 }
